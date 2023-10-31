aaf390b4e5b400ac738af63690d86ecadf3be08a
PhoonThetPine <thetpine254@gmail.com>
2023-10-31 09:53:26 +0800

    Initial Commit

---
+++ Honeymoon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Phoon Thet Pine on 31/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Phoon Thet Pine on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
